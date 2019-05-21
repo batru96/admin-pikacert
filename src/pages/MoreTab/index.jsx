385f07378b0cbc0400bdc0fb87c18fe2b3035b6c
Loc Tran <tbloc@tma.com.vn>
2019-05-21 18:16:41 +0700

    update more tab and select component

--- src/pages/MoreTab/index.jsx
+++ src/pages/MoreTab/index.jsx
@@ -1,6 +1,23 @@
 import React, { Component } from 'react';
+import Select from '../../components/Select';
 
 class MoreTab extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            unit: 'HKD'
+        };
+    }
+    UNITS = [
+        { id: 'HKD', value: 'HKD' },
+        { id: 'USD', value: 'USD' },
+        { id: 'VND', value: 'VND' },
+    ]
+
+    selectUnit = (event) => {
+        this.setState({ unit: event.target.value });
+    }
+
     render() {
         return (
             <div>
@@ -8,20 +25,21 @@ class MoreTab extends Component {
                     <label>Change default credit</label>
                     <input type="text" className="form-control" />
                 </div>
-                <div className="form-group">
-                    <label>How many credits per one US dollar?</label>
-                    <input type="text" className="form-control" />
+                <div>
+                    <div className="form-group">
+                        <label>How many credits?</label>
+                        <input type="text" className="form-control" />
+                    </div>
+                    <Select
+                        name="Unit"
+                        list={this.UNITS}
+                        value={this.state.unit}
+                        onChange={this.selectUnit}
+                    />
                 </div>
             </div>
         );
     }
 }
-/*
-OPTIONS
-[]
-defaultCredits: number
-PRICING: HOW many credits per one US dollar?
 
-
-*/
 export default MoreTab;
